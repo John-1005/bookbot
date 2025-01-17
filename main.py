a0c6cecee0f498783e7e89ebd1f5a3fd79ab87cd
John-1005 <Jtgarza48@gmail.com>
2025-01-17 15:46:54 -0800

    B: Added sorting to finish project

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ def main():
     num_words = get_num_words(text)
     lower_letters = text.lower()
     letters = get_num_letters(lower_letters)
+    organized_letters = organize(letters)
+    
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+    for item in organized_letters:
+        print(f"The {item["character"]} character was found {item["num"]} times")
+    print(f"--- End report ---")
 
 def get_num_words(text):
     words = text.split()
@@ -17,11 +25,20 @@ def get_num_letters(lower_letters):
                 letters[char] = 1
             else:
                 letters[char] += 1
-    print(letters)
     return letters
+
+def organize(letters):
+    sorted = []
+    for let in letters:
+        sorted.append({"character": let, "num": letters[let]})
+    sorted.sort(reverse=True, key=sort_on)
+    print(sorted)
+    return sorted
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+def sort_on(dict_item):
+    return dict_item["num"]
 
 main()
